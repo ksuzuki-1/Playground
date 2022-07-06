abe3726ac109068728be30ab898f85ab95927c92
ksuzuki-1 <ksuzuki5145@gmail.com>
2022-07-06 09:08:56 +0900

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by ksuzuki on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
