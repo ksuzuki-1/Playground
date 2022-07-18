a0d0ec20414ea8e1ea6146f1a52c28b78bae77ba
ksuzuki-1 <ksuzuki5145@gmail.com>
2022-07-18 17:11:24 +0900

    add_animation_and_color_select_for_circlar_progress

--- Playground/Component/CirclarProgress.swift
+++ Playground/Component/CirclarProgress.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct CirclarProgress: View {
     let progress: Int
+    let color: Color
     let isInserve: Bool
     let strokeWidth: CGFloat
+    var lineCap: CGLineCap = .butt
     var angle: Double = -90
     private var from: Double {
         if isInserve {
@@ -26,12 +28,18 @@ struct CirclarProgress: View {
             return Double(progress) / 100
         }
     }
+    private var offset: CGFloat {
+        strokeWidth / 2
+    }
 
     var body: some View {
         Circle()
             .trim(from: from, to: to)
-            .stroke(.blue, lineWidth:  strokeWidth)
+            .stroke(color, style: StrokeStyle(lineWidth: CGFloat(strokeWidth), lineCap: lineCap))
             .rotationEffect(Angle(degrees: angle))
+            .padding(offset)
+            .aspectRatio(1, contentMode: .fit)
+            .animation(.easeOut, value: progress)
     }
 }
 
@@ -39,8 +47,11 @@ struct CirclarProgress_Previews: PreviewProvider {
     static var previews: some View {
         CirclarProgress(
             progress: 50,
+            color: .blue,
             isInserve: false,
-            strokeWidth: 24
+            strokeWidth: 24,
+            lineCap: .round,
+            angle: 90
         )
     }
 }
--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: Int = 0
+    @State private var isStarted = false
+    @State private var timer: Timer?
+    @State private var buttonText = "Start"
+
     var body: some View {
-        CirclarProgress(progress: 67, isInserve: false, strokeWidth: 24)
+        VStack {
+            CirclarProgress(
+                progress: progress,
+                color: .blue,
+                isInserve: false,
+                strokeWidth: 24,
+                lineCap: .round
+            )
+                .background(circularBackground())
+            Text(String(progress))
+            Button(buttonText, action: {
+                if isStarted {
+                    isStarted.toggle()
+                    buttonText = "Start"
+                    progress = 0
+                    timer?.invalidate()
+                    timer = nil
+                    return
+                }
+                isStarted.toggle()
+                buttonText = "Stop"
+                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                    if progress == 100 {
+                        progress = 0
+                    } else {
+                        progress += 10
+                    }
+                }
+            })
+        }
+    }
+
+    @ViewBuilder private func circularBackground() -> some View {
+        Circle()
+            .stroke(lineWidth: 24)
+            .fill(.blue)
+            .opacity(0.5)
+            .padding(12)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
